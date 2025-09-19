c5c719af48f5c9478da93c587d6818d90be35487
newx <newx@NEW>
2025-09-18 22:50:18 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1._5
+{
+    internal class Program
+    {
+        static int Suma(int n)
+        {
+            if (n == 0) return 0;
+            return n + Suma(n - 1);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduce un número");
+            try
+            {
+                int n = int.Parse(Console.ReadLine());
+                if (n < 0)
+                {
+                    Console.WriteLine("El número debe ser no negativo.");
+                }
+                else
+                {
+                    int resultado = Suma(n);
+                    Console.WriteLine($"La suma de {n} desde 1 es {resultado}");
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("No es un número");
+               
+            }
+           
+            
+        }
+    }
+}
